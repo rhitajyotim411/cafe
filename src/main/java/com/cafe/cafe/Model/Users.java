9d2fce8010eb1f52f0d92f2020c54ee9eaec9c3c
Rhitajyoti Mandal <rhitajyotim411@gmail.com>
2025-02-12 14:06:50 +0530

    Users entity added

---
+++ src/main/java/com/cafe/cafe/Model/Users.java
@@ -0,0 +1,47 @@
+package com.cafe.cafe.Model;
+
+import java.io.Serializable;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "users")
+public class Users implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "contact")
+    private String contact;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "passwd")
+    private String passwd;
+
+    @Column(name = "status")
+    private Boolean status;
+
+    @Column(name = "role")
+    private String role;
+}

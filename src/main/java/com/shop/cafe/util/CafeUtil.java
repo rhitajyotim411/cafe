3b0a4444d60e3b290a8291662f43ce00429ee15e
Rhitajyoti Mandal <rhitajyotim411@gmail.com>
2025-02-24 23:52:11 +0530

    JwtUtil added

--- src/main/java/com/shop/cafe/util/CafeUtil.java
+++ src/main/java/com/shop/cafe/util/CafeUtil.java
@@ -1,5 +1,7 @@
 package com.shop.cafe.util;
 
+import java.util.Random;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -10,11 +12,31 @@ public class CafeUtil {
     public static final String REGISTERED_USER = "An user is already registered with this email";
     public static final String REGISTER_SUCCESS = "User successfully registered";
 
+    // Random String Generator
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    // Constructor
     private CafeUtil() {
         // prevents object creation
     }
 
+    // Response builder
     public static ResponseEntity<String> getResponseEntity(String mssg, HttpStatus status) {
         return new ResponseEntity<>("{\"message\":\"" + mssg + "\"}", status);
+    }
+
+    // Random String Generator
+    public static String generateRandomString() {
+        Random random = new Random();
+        int length = random.nextInt(20, 50);
+        StringBuilder result = new StringBuilder(length);
+        int cxlen = CHARACTERS.length();
+
+        for (int i = 0; i < length; i++) {
+            char ch = CHARACTERS.charAt(random.nextInt(cxlen));
+            result.append(ch);
+        }
+
+        return result.toString();
     }
 }
